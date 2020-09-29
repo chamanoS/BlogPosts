14a1e3d15bb400bc336416bad2f6409c8f6e55b1
chamano sidney <chamano.sr@gmail.com>
2020-09-29 13:17:41 +0200

    adding slugify

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const articlesRouter = require('./routes/articles')
+const Article = require('./models/article')
 const app = express()
+
 
 mongoose.connect('mongodb://localhost/blog',
 { useNewUrlParser: true, useUnifiedTopology: true},()=>{
     console.log('Connected to DataBase')
 })
+mongoose.set('useCreateIndex', true)
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/articles', articlesRouter)
 
-app.get('/', (req,res) => {
-    const articles = [{
-        title: 'Test Articles',
-        createdAt: new Date(),
-        description: 'Test description'
-    },{
-        title: 'Test Article 2s',
-        createdAt: new Date(),
-        description: 'Test description 2nd'
-    }]
-
+app.get('/', async (req,res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'})
     res.render('articles/index', { articles: articles })
 })
 
+app.use('/articles', articlesRouter)
 
 app.listen(5000)
